4fe949b080f0f4ad3ee0666b04935115a8b6e132
MariusNWK <marius.nowak@epitech.eu>
2024-07-09 14:29:53 +0200

    wip: tests

---
+++ __tests__/add-note.test.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
+import AddNote from "../components/AddNote";
+
+test("renders AddNote component", () => {
+  expect(1 + 1).toBe(2);
+});
